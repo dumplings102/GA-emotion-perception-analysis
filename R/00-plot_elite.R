7e505ba6a99d0237e99fd08942af7f27abc5f412
Anita Song <anitasong102@outlook.com>
2024-04-25 14:12:24 +0100

    plot edits

--- R/00-plot_elite.R
+++ R/00-plot_elite.R
@@ -2,7 +2,7 @@
 library(tidyverse)
 library(ggdist)
 
-#import distance data from GA preprocessing in python
+#import distance data and separate by distance measure
 dist_df <- read.csv('processed_data/all_elite_distances.csv') %>%
   select(ID,
          TargetEmotion,
@@ -15,8 +15,10 @@ dist_df <- read.csv('processed_data/all_elite_distances.csv') %>%
   mutate(ID = as.factor(ID),
          TargetEmotion = as.factor(TargetEmotion),
          Group = as.factor(Group),
-         distance_measure = as.factor(distance_measure))
+         distance_measure = as.factor(distance_measure)) %>%
+  janitor::clean_names()
 
+#define data summary function for violin plot
 data_summary <- function(x) {
   m <- mean(x)
   ymin <- m-sd(x)
@@ -24,63 +26,60 @@ data_summary <- function(x) {
   return(c(y=m,ymin=ymin,ymax=ymax))
 }
 
-str(dist_df)
-
 #check subject number in each condition
 check <- dist_df %>%
-  group_by(Group, ID) %>%
+  group_by(group, id) %>%
   summarise(n = n()) #count=6 bc 2 distance measures per emotion (2 x 3 = 6)
 
 #visualise average cosine distance from centroid
 ccos.plot <-
   ggplot(dist_df %>%
            filter(distance_measure=='Centroid_cosine_distance'),
-         aes(x = TargetEmotion,
-             y = distance_score,
-             fill = Group)) +
-  geom_violin(col = NA,
-              alpha = .5) +
+         aes(x = target_emotion, y = distance_score, fill = group)) +
+  geom_violin(col = NA, alpha = .5) +
   stat_summary(fun.data = data_summary,
                position = position_dodge(0.9),
                size = .5) +
   scale_fill_manual(values=c("#999999", "#E69F00")) +
-  scale_y_continuous(name = "cosine distance") +
-  scale_x_discrete(name = 'Target Emotion',
+  scale_y_continuous(name = "") +
+  scale_x_discrete(name = '',
                    labels = c('Anger', 'Fear', 'Happiness')) +
-  theme_classic() +
-  ggtitle("Cosine distance from centroid of elite faces (N=42)") +
-  theme(axis.ticks.x = element_blank())
-
-ccos.raincloud <-
-  ggplot(dist_df %>%
-           filter(distance_measure=='Centroid_cosine_distance'),
-         aes(x = TargetEmotion, y = distance_score, fill = Group)) +
-  stat_halfeye(adjust = 0.5,
-               justification = -0.25,
-               .width = 0,
-               point_colour = NA,
-               alpha = .5,
-               trim = F,
-               scale = .6) +
-  geom_boxplot(width = 0.12,
-               outlier.color = NA,
-               alpha = 0.5,
-               position = position_dodge(0.2)) +
-  stat_dots(side = "left",
-            justification = 1.15,
-            binwidth = 0.01,
-            col=NA) +
-  scale_fill_manual(values=c("#999999", "#E69F00")) +
-  scale_y_continuous(name = "cosine distance") +
-  scale_x_discrete(name = 'Target Emotion',
-                   labels = c('Anger', 'Fear', 'Happiness'),
-                   expand = c(0,0)) +
   theme_minimal() +
   theme(panel.grid = element_blank(),
         panel.background = element_rect(fill = "white",
                                         colour = NA),
         plot.background = element_rect(fill = "white")) +
   ggtitle("Cosine distance from centroid of elite faces (N=42)")
+
+ccos.raincloud <-
+  ggplot(dist_df %>%
+           filter(distance_measure=='Centroid_cosine_distance'),
+         aes(x = target_emotion, y = distance_score, fill = group)) +
+  stat_halfeye(adjust = 0.5,
+               justification = -0.3,
+               .width = 0,
+               point_colour = NA,
+               alpha = .5,
+               trim = F,
+               scale = .5) +
+  geom_boxplot(width = 0.2,
+               outlier.color = NA,
+               alpha = 0.5,
+               position = position_dodge(0.3)) +
+  stat_dots(side = "left",
+            justification = 1.2,
+            binwidth = 0.02,
+            col=NA) +
+  scale_fill_manual(name = "Group",
+                    values=c("#999999", "#E69F00")) +
+  scale_y_continuous(name = "Cosine Distance from Centroid") +
+  scale_x_discrete(name = "Target Emotion",
+                   labels = c('Anger', 'Fear', 'Happiness')) +
+  theme_minimal() +
+  theme(panel.grid = element_blank(),
+        panel.background = element_rect(fill = "white", colour = NA),
+        plot.background = element_rect(fill = "white")) +
+  ggtitle("Variability of elite faces (N=42)")
 
 ggsave('plots/centroid_violin.png', ccos.plot)
 ggsave('plots/centroid_raincloud.png', ccos.raincloud)
@@ -89,9 +88,9 @@ ggsave('plots/centroid_raincloud.png', ccos.raincloud)
 ncos.plot <-
   ggplot(dist_df %>%
            filter(distance_measure=='Neutral_cosine_distance'),
-         aes(x = TargetEmotion,
+         aes(x = target_emotion,
              y = distance_score,
-             fill = Group)) +
+             fill = group)) +
   geom_violin(col = NA,
               alpha = .5) +
   stat_summary(fun.data = data_summary,
@@ -105,30 +104,31 @@ ncos.plot <-
   ggtitle("Cosine distance from neutral of elite faces (N=42)") +
   theme(axis.ticks.x = element_blank())
 
-#ncos.raincloud <-
+ncos.raincloud <-
   ggplot(dist_df %>%
            filter(distance_measure=='Neutral_cosine_distance'),
-         aes(x = TargetEmotion, y = distance_score, fill = Group)) +
+         aes(x = target_emotion, y = distance_score, fill = group)) +
   stat_halfeye(adjust = 0.5,
-               justification = -0.25,
+               justification = -0.35,
                .width = 0,
                point_colour = NA,
                alpha = .5,
                trim = F,
-               scale = .6) +
-  geom_boxplot(width = 0.12,
+               scale = .5) +
+  geom_boxplot(width = 0.2,
                outlier.color = NA,
                alpha = 0.5,
-               position = position_dodge(0.2)) +
+               position = position_dodge(0.3)) +
   stat_dots(side = "left",
-            justification = 1.15,
-            binwidth = 0.01,
+            justification = 1.2,
+            binwidth = 0.02,
             col=NA) +
-  scale_fill_manual(values=c("#999999", "#E69F00")) +
-  scale_y_continuous(name = "cosine distance") +
+  scale_fill_manual(name = 'Group',
+                    values=c("#999999", "#E69F00")) +
+  scale_y_continuous(name = "Cosine Distance From Neutral") +
   scale_x_discrete(name = 'Target Emotion',
                    labels = c('Anger', 'Fear', 'Happiness')) +
-  ggtitle("Cosine distance from neutral of elite faces (N=42)") +
+  ggtitle("Intensity of elite faces (N=42)") +
   theme_minimal() +
   theme(panel.grid = element_blank(),
         panel.background = element_rect(fill = "white",
@@ -137,3 +137,4 @@ ncos.plot <-
 
 ggsave('plots/neutral_violin.png', ncos.plot)
 ggsave('plots/neutral_raincloud.png', ncos.raincloud)
+
--- R/01-analyse_elites.R
+++ R/01-analyse_elites.R
@@ -1,3 +1,5 @@
+#load packages
+library(tidyverse)
 library(lme4)
 library(lmerTest)
 library(emmeans)
@@ -6,7 +8,7 @@ library(AICcmodavg)
 library(ggpubr)
 library(effectsize)
 
-#import data and separate by distance measure
+#import distance data and separate by distance measure
 dist_df <- read.csv('processed_data/all_elite_distances.csv') %>%
   select(ID,
          TargetEmotion,
@@ -56,8 +58,14 @@ c.education <- aov(distance_score ~ group+target_emotion+years_of_education,
 c.age_sex_education <- aov(distance_score ~ group+target_emotion+age+sex+
                              years_of_education, c_df) #control age, sex, education
 aictab(list(c.one, c.two, c.int, c.age, c.sex, c.education, c.age_sex_education),
-       modnames = c('one-way', 'two-way', 'interaction',
-                    'age', 'sex', 'education', 'age+sex+education'))
+       modnames = c('one-way',
+                    'two-way',
+                    'interaction',
+                    'age',
+                    'sex',
+                    'education',
+                    'age+sex+education')) #best fitting model = two-way
+
 #check assumptions
 par(mfrow=c(2,2))
 plot(c.two)
@@ -66,31 +74,41 @@ plot(c.two)
 summary(c.two)
 tukey.c.two <- TukeyHSD(c.two)
 tukey.c.two
-eta_squared(c.age_sex_education)
+eta_squared(c.age_sex_education) #effect size, 0.04 - small effect
 
-n.two <- aov(distance_score ~ Group+TargetEmotion, n_df) #two-way
-plot(n.two)
+n.two <- aov(distance_score ~ group+target_emotion, n_df) #two-way neutral model
+plot(n.two) #check assumptions
 par(mfrow=c(1,1))
 summary(n.two)
 tukey.n.two <- TukeyHSD(n.two)
 tukey.n.two
 
-
-c.lmm.controls <- lmer(distance_score ~ group+target_emotion+years_of_education+wasiiq+
-                (1|id), c_df)
+#linear mixed effects model
+c.lmm.controls <- lmer(distance_score ~ group+target_emotion+
+                         years_of_education+wasiiq+
+                (1|id), c_df) #centroid model with controls
 c.lmm <- lmer(distance_score ~ group+target_emotion+
-                     (1|id), c_df)
-c.lmm.noid <- lmer(distance_score ~ group+(1|target_emotion), c_df)
-n.lmm <- lmer(distance_score ~ group+target_emotion+
-                (1|id), n_df)
+                     (1|id), c_df) #centroid model without controls
+c.lmm.noid <- lmer(distance_score ~ group+
+                     (1|target_emotion), c_df) #centroid model with target emo
+                                              ##as random effect instead of id
 
-r.squaredGLMM(c.lmm)
+n.lmm <- lmer(distance_score ~ group+target_emotion+
+                (1|id), n_df) #neutral model
+
+#check proportion of variance explained by the model
+r.squaredGLMM(c.lmm, MuMIn.noUpdateWarning = T)
+##R2m (marginal) variance explained by fixed effects
+###R2c (conditional) variance explained by entire model
 
 plot(c.lmm)
 qqnorm(resid(c.lmm))
 qqline(resid(c.lmm))
 hist(resid(c.lmm))
 
-summary(c.lmm)
-anova(c.lmm, c.two)
+summary(c.lmm.controls)
+anova(c.lmm, c.two, c.lmm.noid, c.lmm.controls) #AIC for two-way ANOVA is lowest
+##is linear mixed model better or two-way ANOVA?
+###LMM with education and IQ as controls is better fit than without??
+
 anova(n.lmm)
